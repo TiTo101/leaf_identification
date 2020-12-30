eeebaf1fd6681d01a07b0a9eeb7269176b745b76
TiTo101 <TiTo101@users.noreply.github.com>
2020-12-30 09:36:01 +0000

    autopep8 action fixes

--- imageclassifier/imageclassifier/live_prediction.py
+++ imageclassifier/imageclassifier/live_prediction.py
@@ -7,19 +7,21 @@ import numpy as np
 from tensorflow.keras.applications import inception_resnet_v2
 from utils import write_image, key_action, init_cam
 
+
 def predict_frame(frame, model, classes):
     '''
     helper function to make predictions which kind of leaf a picture shows
     '''
     # convert from bgr to rgb
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img_preprocessed = inception_resnet_v2.preprocess_input(frame_rgb)  
-    img_preprocessed = img_preprocessed.reshape((1,224,224,3))
+    img_preprocessed = inception_resnet_v2.preprocess_input(frame_rgb)
+    img_preprocessed = img_preprocessed.reshape((1, 224, 224, 3))
     predictions = data_model.predict(img_preprocessed)
     result_dict = dict(zip(classes, predictions.tolist()[0]))
     print(result_dict)
     print(max(result_dict, key=result_dict.get))
     return result_dict
+
 
 saved_model_path = '../models/maple_oak_beech_basemodel_trained.h5'
 data_model = keras.models.load_model(saved_model_path)
@@ -30,11 +32,11 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 0.7
 color = (0, 0, 0)
 thickness = 2
-org = (50,50)
+org = (50, 50)
 
 if __name__ == "__main__":
 
-    os.environ['KMP_DUPLICATE_LIB_OK']='True'
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
     logging.getLogger().setLevel(logging.INFO)
 
@@ -42,7 +44,7 @@ if __name__ == "__main__":
     key = None
 
     try:
-        # q key not pressed 
+        # q key not pressed
         while key != 'q':
             # Capture frame-by-frame
             ret, frame = webcam.read()
@@ -50,25 +52,33 @@ if __name__ == "__main__":
             key = key_action()
 
             # draw a [224x224] rectangle into the middle of the frame
-            cv2.rectangle(frame,(0+48,0+8),(224+48,224+8),(0,0,0),2)     
+            cv2.rectangle(frame, (0 + 48, 0 + 8),
+                          (224 + 48, 224 + 8), (0, 0, 0), 2)
 
             if key == 'space':
                 # write the image without overlay
                 # extract the [224x224] rectangle out of it
-                image = frame[0+8: 224+8, 0+48: 224+48, :]
+                image = frame[0 + 8: 224 + 8, 0 + 48: 224 + 48, :]
                 result_dict = predict_frame(image, data_model, classes)
                 predicted_class = max(result_dict, key=result_dict.get)
-                proba_of_class = np.round(max(result_dict.values()),3)
+                proba_of_class = np.round(max(result_dict.values()), 3)
 
             # displaying results to screen
-            cv2.putText(frame,f'{predicted_class} with probability {proba_of_class}', org, font, fontScale, color, thickness)
-            
+            cv2.putText(
+                frame,
+                f'{predicted_class} with probability {proba_of_class}',
+                org,
+                font,
+                fontScale,
+                color,
+                thickness)
+
             # disable toolbar
             cv2.namedWindow('frame', flags=cv2.WINDOW_GUI_NORMAL)
-            
+
             # display the resulting frame
             cv2.imshow('frame', frame)
-            
+
     finally:
         # when everything done, release the capture
         logging.info('quit webcam')
--- imageclassifier/imageclassifier/utils.py
+++ imageclassifier/imageclassifier/utils.py
@@ -3,14 +3,15 @@ import os
 from datetime import datetime
 import cv2
 
+
 def write_image(out, frame):
     """
     writes frame from the webcam as png file to disk. datetime is used as filename.
     """
     if not os.path.exists(out):
         os.makedirs(out)
-    now = datetime.now() 
-    dt_string = now.strftime("%d_%m_%Y_%H_%M_%S") 
+    now = datetime.now()
+    dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
     filename = f'{out}{dt_string}.png'
     logging.info(f'write image {filename}')
     cv2.imwrite(filename, frame)
@@ -18,11 +19,11 @@ def write_image(out, frame):
 
 def key_action():
     k = cv2.waitKey(1)
-    if k == 113: # q button
+    if k == 113:  # q button
         return 'q'
-    if k == 32: # space bar
+    if k == 32:  # space bar
         return 'space'
-    if k == 112: # p key
+    if k == 112:  # p key
         return 'p'
     return None
 
@@ -37,13 +38,15 @@ def init_cam(width, height):
     # Check success
     if not cap.isOpened():
         raise ConnectionError("Could not open video device")
-    
-    # Set properties. Each returns === True on success (i.e. correct resolution)
+
+    # Set properties. Each returns === True on success (i.e. correct
+    # resolution)
     assert cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     assert cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     return cap
 
+
 def add_text(text, frame):
     # Put some rectangular box on the image
-    cv2.putText( s) # Funktion füllen
+    cv2.putText(s)  # Funktion füllen
     return frame
